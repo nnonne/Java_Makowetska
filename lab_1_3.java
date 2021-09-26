a0a87b42916b62c0e81db518bf94dc8d6b9c996d
nnonne <70632388+nnonne@users.noreply.github.com>
2021-09-26 23:12:00 +0300

    classwork 1

---
+++ lab_1_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class lab_1_3 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+	    System.out.println("Input x: ");
+	    float x = in.nextFloat();
+	    System.out.println("Input y: ");
+	    float y = in.nextFloat();
+	    System.out.println("Input z: ");
+	    float z = in.nextFloat();
+	    in.close();
+	    System.out.println(x);
+	    System.out.println(y);
+	    System.out.println(z);
+	}
+}
